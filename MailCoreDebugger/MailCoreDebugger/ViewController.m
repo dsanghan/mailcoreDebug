2109eb1cbde203bfc2bcc2901374e617e6b2d064
Dev Sanghani <dev_sanghani@yahoo.com>
2016-05-18 14:50:10 +0530

    Add some more emls

--- MailCoreDebugger/MailCoreDebugger/ViewController.m
+++ MailCoreDebugger/MailCoreDebugger/ViewController.m
@@ -20,21 +20,33 @@
 
 @implementation ViewController
 
+- (IBAction)didPressMessageButton:(id)sender {
+    [self loadMessageForTag:[(NSButton *)sender tag]];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [[NSUserDefaults standardUserDefaults] setBool:TRUE forKey:@"WebKitDeveloperExtras"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 
     // Do any additional setup after loading the view.
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"message" ofType:@"eml"];
-    NSData *data = [NSData dataWithContentsOfFile:path];
-    MCOMessageParser *parser = [MCOMessageParser messageParserWithData:data];
-    NSString *html = [parser htmlRenderingWithDelegate:nil];
-    [[_webview mainFrame] loadHTMLString:html baseURL:nil];
+    [self loadMessageForTag:1];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+- (void)loadMessageForTag:(NSInteger)tag {
+    NSString *message = [NSString stringWithFormat:@"message%lu", tag];
+    NSString *path = [[NSBundle mainBundle] pathForResource:message ofType:@"eml"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    MCOMessageParser *parser = [MCOMessageParser messageParserWithData:data];
+    NSString *html = [parser htmlRenderingWithDelegate:nil];
+    [[_webview mainFrame] loadHTMLString:html baseURL:nil];
 }
 
 @end
