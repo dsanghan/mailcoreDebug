b900c60e450f27c7c65323f9ee415f3cc20c462c
Dev Sanghani <dev_sanghani@yahoo.com>
2016-05-18 13:54:16 +0530

    [MailCore] Integrate the library and add an example EML

--- MailCoreDebugger/MailCoreDebugger/ViewController.m
+++ MailCoreDebugger/MailCoreDebugger/ViewController.m
@@ -8,12 +8,27 @@
 
 #import "ViewController.h"
 
+#import <MailCore/MailCore.h>
+
+#import <WebKit/WebKit.h>
+
+@interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet WebView *webview;
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"message" ofType:@"eml"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    MCOMessageParser *parser = [MCOMessageParser messageParserWithData:data];
+    NSString *html = [parser htmlRenderingWithDelegate:nil];
+    [[_webview mainFrame] loadHTMLString:html baseURL:nil];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
